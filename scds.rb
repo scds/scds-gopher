0f95cf75093fd263d272ea17f30bfc6efbe8e2a1
jbfink <john.fink@gmail.com>
2014-01-21 16:39:48 -0500

    Fleshing out scds.rb

--- scds.rb
+++ scds.rb
@@ -1,6 +1,29 @@
+require 'gopher2000'
+set :host, '0.0.0.0'
 set :port, 7070
 
-route '/' do
-	"WELCOME TO GOPPPHERRRR"
+route '/gopher' do
+	"Greetings from Gopher 2000!"
 end
+
+route '/' do
+	render :index
+end
+
+menu :index do
+	text 'Welcome to the Sherman Centre for Digital Scholarship'
+
+	br(2)
+
+	link 'About the Centre', '/about'
+	br
+
+	link 'Map of the Centre', '/map'
+	br
+
+	link "Who's Who in the Centre", '/who'
+end
+
+
+
 
